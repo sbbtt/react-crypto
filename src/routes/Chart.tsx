11486850b0a51eed9ac77ae2219503a62b2dd3b9
석주 <54101187+sbbtt@users.noreply.github.com>
2022-11-22 16:42:35 +0900

    data not found (400 error) 처리
    fetching 내부 !response.ok 체크 후 throw 분기 처리

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -3,19 +3,60 @@ import { useOutletContext } from "react-router";
 import { useRecoilValue } from "recoil";
 import { isDarkAtom } from "../atoms";
 import { fetchCoinHistory } from "./api";
+import ApexChart from 'react-apexcharts'
+
 
 type ChartProps={
   coinId: string;
+}
+
+interface Idata {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
 }
 
 function Chart() {
   const {coinId} =useOutletContext<ChartProps>(); // {food: "pizza"}
   console.log('coinid:: ',coinId)
   const isDark = useRecoilValue(isDarkAtom);
-   const {isLoading, data} = useQuery(['ohlcv', coinId], ()=>fetchCoinHistory(coinId))
+   const {isLoading, data} = useQuery<Idata[]>(['ohlcv', coinId], ()=>fetchCoinHistory(coinId))
+   console.log('data값: ',data)
   return (
-    <h1>{coinId}</h1>
-  )
+    <div>
+      {isLoading ? "loading chart.." : <ApexChart type="line" 
+      series={[
+        {
+          name: 'chart',
+          data: 
+          data?.map((x)=>x.close) as number[]
+        },
+      ]}
+      options={{
+        theme:{
+          mode:'dark'
+        },
+        chart:{
+            height: 500,
+            width: 500,
+            background: 'transparent'
+        },
+        stroke:{
+          // width: 1
+        },
+        // fill: {type: 'gradient', gradient:{gradientToColors:['blue'], stops:[0, 1]}},
+        // colors:['red']
+
+      }} 
+      />
+      }
+    </div>
+  );
 }
 
 export default Chart
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -16,6 +16,13 @@ export function fetchCoinTickers(coinId:string|undefined){
         );
 }
 export function fetchCoinHistory(coinId:string|undefined){
+    // const endDate = Math.floor(Date.now()/1000)
+    // const startDate = endDate - 60 * 60 * 23;
     return fetch(`https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}`)
-    .then(response => response.json());
+    .then(response => {
+        if(!response.ok) {
+            throw new Error (`에러코드 ${response.status} 발생했습니다.`)
+        }
+        return response.json()
+    })
 }
